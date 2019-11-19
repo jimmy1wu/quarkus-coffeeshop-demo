b40e8ed553449a619528c5ef9abab067e999ac10
Sandra Hayward <cotton_glow@hotmail.com>
2019-11-19 15:17:20 +0000

    Added namespaces to the kubernetes script

--- apply-kubernetes.bat
+++ apply-kubernetes.bat
@@ -1,11 +1,12 @@
-kubectl apply -f zookeeper-deployment.yaml
-kubectl apply -f kafka-deployment.yaml
-kubectl apply -f barista-http/deployment.yml
-kubectl apply -f barista-kafka/deployment.yml
-kubectl apply -f coffeeshop-service/deployment.yml
+kubectl create ns coffeeshop-demo
+kubectl apply -f zookeeper-deployment.yaml --namespace=coffeeshop-demo
+kubectl apply -f kafka-deployment.yaml --namespace=coffeeshop-demo
+kubectl apply -f barista-http/deployment.yml --namespace=coffeeshop-demo
+kubectl apply -f barista-kafka/deployment.yml --namespace=coffeeshop-demo
+kubectl apply -f coffeeshop-service/deployment.yml --namespace=coffeeshop-demo
 
-kubectl apply -f zookeeper-service.yaml
-kubectl apply -f kafka-service.yaml
-kubectl apply -f barista-http/service.yml
-kubectl apply -f barista-kafka/service.yml
-kubectl apply -f coffeeshop-service/service.yml
+kubectl apply -f zookeeper-service.yaml --namespace=coffeeshop-demo
+kubectl apply -f kafka-service.yaml --namespace=coffeeshop-demo
+kubectl apply -f barista-http/service.yml --namespace=coffeeshop-demo
+kubectl apply -f barista-kafka/service.yml --namespace=coffeeshop-demo
+kubectl apply -f coffeeshop-service/service.yml --namespace=coffeeshop-demo
