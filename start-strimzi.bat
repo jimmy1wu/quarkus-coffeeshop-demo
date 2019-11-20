006c259ba769c0318421f550ab5ea8ee788a49d2
Sandra Hayward <cotton_glow@hotmail.com>
2019-11-20 09:44:10 +0000

    Added strimzi operators, updated existing kubernetes apply and delete files and updated the Dockerfiles

---
+++ start-strimzi.bat
@@ -0,0 +1,7 @@
+kubectl create ns kafka
+kubectl create ns coffeeshop-demo
+kubectl apply -f strimzi-0.14.0/cluster-operator/ -n kafka
+kubectl apply -f strimzi-0.14.0/cluster-operator/020-RoleBinding-strimzi-cluster-operator.yaml -n coffeeshop-demo
+kubectl apply -f strimzi-0.14.0/cluster-operator/032-RoleBinding-strimzi-cluster-operator-topic-operator-delegation.yaml -n coffeeshop-demo
+kubectl apply -f strimzi-0.14.0/cluster-operator/031-RoleBinding-strimzi-cluster-operator-entity-operator-delegation.yaml -n coffeeshop-demo
+kubectl apply -f kafka-strimzi.yml -n coffeeshop-demo
