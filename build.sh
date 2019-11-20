b4b46c2a1c8c0e115b430106f50f79aaf094cf12
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-11-20 10:14:05 +0000

    Fix path seperators

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -f barista-kafka\Dockerfile -t barista-kafka .
-docker build -f barista-http\Dockerfile -t barista-http .
-docker build -f coffeeshop-service\Dockerfile -t coffeeshop-service .
+docker build -f barista-kafka/Dockerfile -t barista-kafka .
+docker build -f barista-http/Dockerfile -t barista-http .
+docker build -f coffeeshop-service/Dockerfile -t coffeeshop-service .
