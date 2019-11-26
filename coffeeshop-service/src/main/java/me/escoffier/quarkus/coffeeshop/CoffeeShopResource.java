500a8aff961335d1111ddf9d9f2d2dad451a7cb7
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-11-26 13:41:02 +0000

    Basic libertification

--- coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/CoffeeShopResource.java
+++ coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/CoffeeShopResource.java
@@ -1,13 +1,6 @@
 package me.escoffier.quarkus.coffeeshop;
 
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Emitter;
-import io.smallrye.reactive.messaging.annotations.Stream;
-import me.escoffier.quarkus.coffeeshop.http.BaristaService;
-import me.escoffier.quarkus.coffeeshop.model.Beverage;
-import me.escoffier.quarkus.coffeeshop.model.Order;
-import me.escoffier.quarkus.coffeeshop.model.PreparationState;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import java.util.UUID;
 
 import javax.inject.Inject;
 import javax.json.bind.Jsonb;
@@ -16,8 +9,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.UUID;
-import java.util.concurrent.CompletionStage;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.smallrye.reactive.messaging.annotations.Channel;
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import me.escoffier.quarkus.coffeeshop.http.BaristaService;
+import me.escoffier.quarkus.coffeeshop.model.Beverage;
+import me.escoffier.quarkus.coffeeshop.model.Order;
+import me.escoffier.quarkus.coffeeshop.model.PreparationState;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
--- coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
+++ coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
@@ -1,15 +1,15 @@
 package me.escoffier.quarkus.coffeeshop.dashboard;
 
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Stream;
-import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
-import org.reactivestreams.Publisher;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
+import org.reactivestreams.Publisher;
+
+import io.smallrye.reactive.messaging.annotations.Channel;
 
 @Path("/queue")
 public class BoardResource {
