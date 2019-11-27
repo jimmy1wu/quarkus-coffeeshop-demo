2d7b7dad2f20de5941041066972a984667548777
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-11-27 16:07:15 +0000

    Increase timeout on test

--- ci-test.sh
+++ ci-test.sh
@@ -14,7 +14,7 @@ else
 fi
 
 # Test order completion
-eventStream=`curl -s --max-time 5.5 http://localhost:8080/queue | grep 'data'`
+eventStream=`curl -s --max-time 8 http://localhost:8080/queue | grep 'data'`
 responseRegex='"state" *: *"READY"'
 
 if [[ "$eventStream" =~ $responseRegex ]]; then
