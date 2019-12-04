9f17d46f5cf720bd14731909cfb913803ba93ab1
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-12-04 09:13:19 +0000

    sse stuff - not working

--- coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
+++ coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
@@ -3,6 +3,7 @@ package me.escoffier.quarkus.coffeeshop.dashboard;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,42 +23,35 @@ import org.slf4j.LoggerFactory;
 public class BoardResource {
 
     private static Logger logger = LoggerFactory.getLogger(BoardResource.class);
-
-    private AtomicInteger sseId = new AtomicInteger(0);
+    private SseBroadcaster broadcaster;
+    private Sse sse;
 
     @Context
-    private Sse sse;
-    private SseBroadcaster broadcaster;
-
-    private SseBroadcaster getBroadcaster() {
-        if (broadcaster == null) {
-            broadcaster = sse.newBroadcaster();
-        }
-        return broadcaster;
+    public void setSse( Sse sse) {
+        this.sse = sse;
+        this.broadcaster = sse.newBroadcaster();
+        logger.info("setSse: sse " + sse + ", broadcaster " + broadcaster);
     }
 
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public void subscribeToQueue(@Context SseEventSink eventSink) {
         logger.info("Client subscribed to SSE events " + eventSink);
-        // OutboundSseEvent queueEvent =
-        // sse.newEvent("dummy","{\"order\":{\"name\":\"Dummy\",\"orderId\":\"FAKE_ONE\",\"product\":\"test\"},\"state\":\"IN_QUEUE\"}");
-        // eventSink.send(queueEvent);
-
-        getBroadcaster().register(eventSink);
+        broadcaster.register(eventSink);
     }
 
     @Incoming("beverages")
     public void processQueue(String data) {
         logger.info("GOT: " + data);
-        OutboundSseEvent queueEvent = sse.newEventBuilder()
-                                        .name("beverage")
-                                        .id(Integer.toString(sseId.getAndIncrement()))
-                                        .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                                        .build();
+        if (broadcaster == null) {
+            logger.info("broadcaster == null, don't seem to have context yet");
+            return;
+        } 
+
+        OutboundSseEvent queueEvent = sse.newEvent(data);
         if (queueEvent != null) {
-            logger.info("About to send SSE: " + queueEvent + "to broadcaster: " + getBroadcaster());
-            getBroadcaster().broadcast(queueEvent);
+            logger.info("About to send SSE: " + queueEvent + "to broadcaster: " + broadcaster);
+            broadcaster.broadcast(queueEvent);
         } else {
             logger.info("Created a null event!");
         }
