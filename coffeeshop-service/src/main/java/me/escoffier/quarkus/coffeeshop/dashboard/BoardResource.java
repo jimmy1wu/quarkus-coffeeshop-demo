4339b7baf82f9ec1f8fb535905d24c5efed9438b
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-12-04 09:45:59 +0000

    websocket working!

--- coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
+++ coffeeshop-service/src/main/java/me/escoffier/quarkus/coffeeshop/dashboard/BoardResource.java
@@ -1,59 +1,53 @@
 package me.escoffier.quarkus.coffeeshop.dashboard;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.sse.OutboundSseEvent;
-import javax.ws.rs.sse.Sse;
-import javax.ws.rs.sse.SseBroadcaster;
-import javax.ws.rs.sse.SseEventSink;
+import javax.websocket.CloseReason;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Path("/queue")
+@ServerEndpoint("/services/queue")
 @ApplicationScoped
 public class BoardResource {
 
     private static Logger logger = LoggerFactory.getLogger(BoardResource.class);
-    private SseBroadcaster broadcaster;
-    private Sse sse;
+    private List<Session> clientSessions = Collections.synchronizedList(new ArrayList<>());
 
-    @Context
-    public void setSse( Sse sse) {
-        this.sse = sse;
-        this.broadcaster = sse.newBroadcaster();
-        logger.info("setSse: sse " + sse + ", broadcaster " + broadcaster);
+    @OnOpen
+    public void subscribeToQueue(Session session, EndpointConfig ec) {
+        logger.info("Websocket opened " + session.getId());
+        clientSessions.add(session);
     }
 
-    @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void subscribeToQueue(@Context SseEventSink eventSink) {
-        logger.info("Client subscribed to SSE events " + eventSink);
-        broadcaster.register(eventSink);
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        logger.info("Websocket session closed " + session.getId());
+        clientSessions.remove(session);
     }
 
     @Incoming("beverages")
     public void processQueue(String data) {
         logger.info("GOT: " + data);
-        if (broadcaster == null) {
-            logger.info("broadcaster == null, don't seem to have context yet");
-            return;
-        } 
 
-        OutboundSseEvent queueEvent = sse.newEvent(data);
-        if (queueEvent != null) {
-            logger.info("About to send SSE: " + queueEvent + "to broadcaster: " + broadcaster);
-            broadcaster.broadcast(queueEvent);
-        } else {
-            logger.info("Created a null event!");
+        synchronized (clientSessions) {
+            for (Session clientSession : clientSessions) {
+                try {
+                    clientSession.getBasicRemote().sendText(data);
+                } catch (IOException e) {
+                    logger.error("Error sending message to websocket client " + clientSession.getId(), e);
+                }
+            }
         }
     }
 }
