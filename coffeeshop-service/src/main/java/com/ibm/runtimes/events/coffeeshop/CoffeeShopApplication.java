53516996ffcb457cfe9317ff4dea844e5d4092f3
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-12-18 08:21:05 +0000

    Rename packages

---
+++ coffeeshop-service/src/main/java/com/ibm/runtimes/events/coffeeshop/CoffeeShopApplication.java
@@ -0,0 +1,9 @@
+package com.ibm.runtimes.events.coffeeshop;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class CoffeeShopApplication extends Application {
+
+}
