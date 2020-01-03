5a7614d03b1aca7ebf086b75f84110988d3c0d98
Matthew Wilson <matthew.wilson@uk.ibm.com>
2020-01-03 13:20:59 +0000

    Filter completd orders in separate stream

--- barista-kafka/src/main/java/com/ibm/runtimes/events/coffeeshop/KafkaBarista.java
+++ barista-kafka/src/main/java/com/ibm/runtimes/events/coffeeshop/KafkaBarista.java
@@ -1,17 +1,5 @@
 package com.ibm.runtimes.events.coffeeshop;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-
-import com.ibm.runtimes.events.coffeeshop.PreparationState.State;
-
-import javax.inject.Inject;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Random;
@@ -20,6 +8,20 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import com.ibm.runtimes.events.coffeeshop.PreparationState.State;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Processor;
 
 @ApplicationScoped
 public class KafkaBarista {
@@ -31,17 +33,27 @@ public class KafkaBarista {
     private Random random = new Random();
 
     private Set<Order> completedOrders = Collections.synchronizedSet(new HashSet<Order>());
-    
-    private Jsonb jsonb = JsonbBuilder.create();;
+
+    private Jsonb jsonb = JsonbBuilder.create();
 
     @Incoming("orders")
+    @Outgoing("pendingOrders")
+    public Processor<Message<String>,Message<String>> filterCompletedOrders() {
+        return ReactiveStreams.<Message<String>>builder().filter(message -> {
+            Order order = jsonb.fromJson(message.getPayload(), Order.class);
+            if (completedOrders.contains(order)) {
+                System.out.println("Order " + order.getOrderId() + " already completed, filtering.");
+                return false;
+            }
+            message.ack();
+            return true;
+        }).buildRs();
+    }
+
+    @Incoming("pendingOrders")
     @Outgoing("queue")
     public CompletionStage<String> prepare(String message) {
         Order order = jsonb.fromJson(message, Order.class);
-        if (completedOrders.contains(order)) {
-            System.out.println("Order " + order.getOrderId() + " has already been completed, skipping.");
-            return null;
-        }
         System.out.println("Barista " + name + " is going to prepare a " + order.getProduct());
         return makeIt(order)
                 .thenApply(beverage -> PreparationState.ready(order, beverage));
@@ -58,20 +70,14 @@ public class KafkaBarista {
     private CompletionStage<Beverage> makeIt(Order order) {
         return CompletableFuture.supplyAsync(() -> {
             System.out.println("Preparing a " + order.getProduct());
-            prepare();
-            if (completedOrders.contains(order)) {
-                System.out.println("Oops, someone else prepared order id " + order.getOrderId() + " I'll throw this one away.");
-                return null;
-            }
+            prepareCoffee();
             return new Beverage(order, name);
         }, executor);
     }
 
-
-
     private Executor executor = Executors.newSingleThreadExecutor();
 
-    private void prepare() {
+    private void prepareCoffee() {
         try {
             Thread.sleep(random.nextInt(5000));
         } catch (InterruptedException e) {
